ee63df2534fb797a08ba403f908eaee4402d1368
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-02 12:10:51 +0900

    Add input loop logic

--- akka-supervisor/src/main/scala/supervisor/Main.scala
+++ akka-supervisor/src/main/scala/supervisor/Main.scala
@@ -1,9 +1,22 @@
 package supervisor
 
+import scala.io.StdIn._
+
 import com.typesafe.scalalogging.LazyLogging
 
 object Main extends App with LazyLogging {
 
+  def loop(line: String): String = {
+    line match {
+      case "q" =>
+        println("Exit")
+        "exist"
+      case _ =>
+        // actor ! line
+        loop(readLine())
+    }
   }
 
+  logger.info("Start")
+  loop(readLine())
 }
