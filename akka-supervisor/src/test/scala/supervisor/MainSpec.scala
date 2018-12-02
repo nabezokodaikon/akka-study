dc341b59a79e339afaca36dafc9d0cc55c81a1ea
nabezokodaikon <nabezoko.daikon@gmail.com>
2018-12-02 11:07:30 +0900

    Add supervisor project

---
+++ akka-supervisor/src/test/scala/supervisor/MainSpec.scala
@@ -0,0 +1,9 @@
+package supervisor
+
+import org.scalatest.FunSuite
+
+class MainSpec extends FunSuite {
+  test("helloWorld") {
+    assert(Main.helloWorld("nabezokodaikokn") == "Hello nabezokodaikokn!")
+  }
+}
